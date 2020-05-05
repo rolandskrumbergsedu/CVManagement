ab407c8ae784b7e0556ec8b4bd0654e4a979af30
Rolands Krumbergs <rolands.krumbergs@outlook.com>
2020-05-05 12:45:26 +0300

    Downgraded to jQuery 3.4.1

--- CV.Management.Web/App_Start/BundleConfig.cs
+++ CV.Management.Web/App_Start/BundleConfig.cs
@@ -27,39 +27,6 @@ namespace CV.Management.Web
                       "~/Content/site.css",
                       "~/Content/main.css"));
 
-            bundles.Add(new StyleBundle("~/legacy/css/style").Include(
-                      "~/Content/Legacy/style.css"));
-
-            bundles.Add(new StyleBundle("~/legacy/css/croppie").Include(
-                      "~/Content/Legacy/croppie.css"));
-
-            bundles.Add(new StyleBundle("~/legacy/css/bootstrap-datepicker").Include(
-                      "~/Content/Legacy/bootstrap-datetimepicker.min.css"));
-
-            bundles.Add(new ScriptBundle("~/legacy/js/jquery").Include(
-                "~/Content/Legacy/js/vendor/jquery.min.js"));
-
-            bundles.Add(new ScriptBundle("~/legacy/js/moment").Include(
-                "~/Content/Legacy/js/vendor/moment.js"));
-
-            bundles.Add(new ScriptBundle("~/legacy/js/bootstrap").Include(
-                "~/Content/Legacy/js/vendor/bootstrap.min.js"));
-
-            bundles.Add(new ScriptBundle("~/legacy/js/bootstrap-datepicker").Include(
-               "~/Content/Legacy/js/vendor/bootstrap-datetimepicker.min.js"));
-
-            bundles.Add(new ScriptBundle("~/legacy/js/jquery-scrollbar").Include(
-               "~/Content/Legacy/js/vendor/jquery.mCustomScrollbar.concat.min.js"));
-
-            bundles.Add(new ScriptBundle("~/legacy/js/jquery-mask").Include(
-               "~/Content/Legacy/js/jquery.mask.js"));
-
-            bundles.Add(new ScriptBundle("~/legacy/js/autosize").Include(
-               "~/Content/Legacy/js/autosize.js"));
-
-            bundles.Add(new ScriptBundle("~/legacy/js/croppie").Include(
-               "~/Content/Legacy/js/croppie.js"));
-
             bundles.Add(new ScriptBundle("~/bundles/app").Include(
                 "~/Scripts/app.js"));
 
