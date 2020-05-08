0cddc10a88029ee1ace1ab7a385adaef9d02dee4
Rolands Krumbergs <rolands.krumbergs@outlook.com>
2020-05-08 13:08:59 +0300

    Profile deletion added

--- CV.Management.Web/Controllers/AdminController.cs
+++ CV.Management.Web/Controllers/AdminController.cs
@@ -14,6 +14,7 @@ namespace CV.Management.Web.Controllers
         private readonly TelemetryClient telemetry = new TelemetryClient();
 
         // GET: Admin
+        [HttpGet]
         public ActionResult Overview()
         {
             using (var db = new ProfileInformationDbContext())
@@ -32,6 +33,62 @@ namespace CV.Management.Web.Controllers
                 return View(viewmodel);
             }
 
+        }
+
+        [HttpGet]
+        public ActionResult ConfirmUserDelete(string id)
+        {
+            using (var db = new ProfileInformationDbContext())
+            {
+                telemetry.TrackPageView("ConfirmUserDelete");
+
+                var currentUsername = User.Identity.Name;
+
+                telemetry.TrackEvent("ConfirmUserDelete", new Dictionary<string, string> { { "User", currentUsername } });
+
+                var profile = db.Profiles.FirstOrDefault(x => x.ProfileId.ToString() == id);
+
+                var userModel = new ConfirmUserDeleteViewModel
+                {
+                    Name = currentUsername,
+                    Id = id,
+                    Email = profile.Email,
+                    FullName = profile.FullName,
+                    LinkedInLink = profile.LinkedInLink,
+                    Project = profile.Project,
+                    Username = profile.Username
+                };
+
+                return View(userModel);
+            }
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteUser(ConfirmUserDeleteViewModel model)
+        {
+            using (var db = new ProfileInformationDbContext())
+            {
+                telemetry.TrackPageView("DeleteUser");
+
+                var currentUsername = User.Identity.Name;
+
+                telemetry.TrackEvent("DeleteUser", new Dictionary<string, string> { { "User", currentUsername } });
+
+                var profile = db.Profiles
+                    .Include("Educations")
+                    .Include("AdditionalCourses")
+                    .Include("Languages")
+                    .Include("Companies.Positions.KeyTasks")
+                    .Include("Memberships")
+                    .Include("AdditionalFiles")
+                    .FirstOrDefault(x => x.ProfileId.ToString() == model.Id);
+                
+                db.Profiles.Remove(profile);
+                db.SaveChanges();
+
+                return RedirectToAction("Overview"); 
+            }
         }
 
         private AdminOverviewViewModel ProfilesToViewModel(string name, List<Profile> profiles)
---
+++ CV.Management.Web/Models/ConfirmUserDeleteViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CV.Management.Web.Models
+{
+    public class ConfirmUserDeleteViewModel
+    {
+        public string Name { get; set; }
+        public string Id { get; set; }
+        public string Username { get; set; }
+        public string FullName { get; set; }
+        public string LinkedInLink { get; set; }
+        public string Email { get; set; }
+        public string Project { get; set; }
+    }
+}
