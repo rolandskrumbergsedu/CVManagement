22754f362723b339e8de6202619215b16ff7c2ce
Rolands Krumbergs <rolands.krumbergs@outlook.com>
2020-05-27 11:32:43 +0300

    Added error page

---
+++ CV.Management.Web/Controllers/ErrorController.cs
@@ -0,0 +1,36 @@
+﻿using CV.Management.Web.Models;
+using Microsoft.ApplicationInsights;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CV.Management.Web.Controllers
+{
+    public class ErrorController : Controller
+    {
+        private readonly TelemetryClient _telemetry = new TelemetryClient();
+
+        [HttpGet]
+        public ViewResult Index()
+        {
+            var traceId = Guid.NewGuid().ToString();
+
+            _telemetry.TrackEvent("ErrorPage", new Dictionary<string, string> { { "TraceId", traceId } });
+
+            var viewModel = new ErrorViewModel
+            {
+                TraceId = traceId
+            };
+
+            return View(viewModel);
+        }
+
+        [HttpGet]
+        public ViewResult NotFound()
+        {
+            return View("NotFound");
+        }
+    }
+}
