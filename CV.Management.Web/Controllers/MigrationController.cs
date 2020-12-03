69d6009257c6b9a4c3058d0ea6217c03e1a94ea0
Rolands Krumbergs <rolands.krumbergs@outlook.com>
2020-12-03 09:27:50 +0200

    Added controller to migrate profiles in English

---
+++ CV.Management.Web/Controllers/MigrationController.cs
@@ -0,0 +1,33 @@
+﻿using CV.Management.Web.DbContexts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+
+namespace CV.Management.Web.Controllers
+{
+    public class MigrationController : ApiController
+    {
+        [HttpGet]
+        [Route("api/migrations/profileLanguage")]
+        public HttpResponseMessage MigrateProfileDefaultLanguage()
+        {
+            using (var db = new ProfileInformationDbContext())
+            {
+                var emptyProfiles = db.Profiles.Where(x => x.Language == null);
+
+                foreach (var profile in emptyProfiles)
+                {
+                    profile.Language = "en";
+                }
+
+                db.SaveChanges();
+            }
+
+            return Request.CreateResponse(HttpStatusCode.OK);
+        }
+    }
+}
